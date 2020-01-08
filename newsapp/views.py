5af28a12dcf58115c5772a835cca37e4015d4831
Alex Preston <alexpreston@Alexs-MacBook-Pro-2.local>
2020-01-08 11:47:08 -0500

    Correctly modified URL mapping, I was adding URLs in the root folder (mysite), instead of the actual app, whoops

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+#def home(request):
+#    return render(request, 'home.html')
+
+
+
+# importing api 
+from django.shortcuts import render 
+from newsapi import NewsApiClient 
+  
+# Create your views here.  
+def home(request): 
+      
+    newsapi = NewsApiClient(api_key ='7890f99f817b40a0a587325193ca0933') 
+    top = newsapi.get_top_headlines(sources ='business-insider') 
+  
+    l = top['articles'] 
+    desc =[] 
+    news =[] 
+    img =[] 
+  
+    for i in range(len(l)): 
+        f = l[i] 
+        news.append(f['title']) 
+        desc.append(f['description']) 
+        img.append(f['urlToImage']) 
+    mylist = zip(news, desc, img) 
+  
+    return render(request, 'home.html', context ={"mylist":mylist}) 

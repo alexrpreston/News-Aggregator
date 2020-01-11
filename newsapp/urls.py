861f481e6085ff18b9b1d6d02ffc419ec21ff13e
Alex Preston <alexpreston@Alexs-MacBook-Pro-local>
2020-01-11 14:03:43 -0500

    Created a basic model, now just need to save the scraped data to the model

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.techCrunch, name='techCrunch'),
-    path('', views.wallStreetJournal, name='wallStreetJournal'),
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -30,21 +30,4 @@ def techCrunch(request):
     
     return render(request, 'newsapp/home.html', context ={"TClist":TClist}) 
 
-def wallStreetJournal(request): 
-    newsapi = NewsApiClient(api_key ='7890f99f817b40a0a587325193ca0933') 
-    top = newsapi.get_top_headlines(sources ='the-wall-street-journal') 
-  
-    l = top['articles'] 
-    desc =[] 
-    news =[] 
-    img =[] 
-  
-    for i in range(len(l)): 
-        f = l[i] 
-        news.append(f['title']) 
-        desc.append(f['description']) 
-        img.append(f['urlToImage']) 
-    WSJlist = zip(news, desc, img) 
-    
-    return render(request, 'newsapp/home.html', context ={"WSJlist":WSJlist})
 
